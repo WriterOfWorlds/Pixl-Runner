be4b8b55a308b711584047032f4bc678661f1423
WriterOfWorlds <kamalualston@gmail.com>
2022-05-21 23:30:53 -0700

    Added janky install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+curl https://bootstrap.pypa.io/get-pip.py --output ~/Downloads/get-pip.py
+python3 ~/Downloads/get-pip.py
+pip install Pillow
+rm ~/Downloads/get-pip.py
+echo You can now run main.py
